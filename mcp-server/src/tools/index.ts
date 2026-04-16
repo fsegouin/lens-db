a34ad9cba92cd1520d3e6b89a247c2983b49a0d1
Florent Segouin <florent@segouin.me>
2026-04-16 11:43:17 +0200

    feat: add tools index and AI SDK tool wrappers

---
+++ mcp-server/src/tools/index.ts
@@ -0,0 +1,7 @@
+export { searchCameras, searchCamerasSchema } from "./search-cameras.js";
+export { searchLenses, searchLensesSchema } from "./search-lenses.js";
+export { getCameraDetails, getCameraDetailsSchema } from "./get-camera-details.js";
+export { getLensDetails, getLensDetailsSchema } from "./get-lens-details.js";
+export { getPrice, getPriceSchema } from "./get-price.js";
+export { getSystemInfo, getSystemInfoSchema } from "./get-system-info.js";
+export { getCompatibleLenses, getCompatibleLensesSchema } from "./get-compatible-lenses.js";
